688f8f91a7206eea0c1e5f9c461b0f17eef37c93
Tom <tomtarpeydev@gmail.com>
2019-08-04 15:45:41 +0100

    added a get handler for the tasks endpoint

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify, abort, make_response
 
 app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy food',
+        'description': u'Milk, Orange Juice, Pizza',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Flask',
+        'description': u'Read the flask docs',
+        'done': False
+    }
+]
 
 @app.route('/')
 def index():
     return "Hello, World!"
+
+@app.route('/todo/api/v1/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
